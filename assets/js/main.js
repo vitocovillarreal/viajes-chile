6f0ee50b62d4a9b23a7379b27349686381e757e6
Victor Villarreal <victor.villarrealj@gmail.com>
2022-12-18 19:19:26 -0300

    ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 const myCarouselElement = document.querySelector('#myCarousel')
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 1000,
   wrap: false
-})
+}) 
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 $(".enviar").click(function(){
-    alert("Mensaje enviado correctamente")
+  alert("El correo fue enviado correctamente")
 })
